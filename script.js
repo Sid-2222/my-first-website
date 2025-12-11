ba707adf3c493ce9df15191ccfc5d36d165503ff
Your Name <you@example.com>
2025-12-11 12:17:29 -0500

    buttton_likeCount

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 console.log("hello");
 console.log("World");
-console.log("just checking commit")
+console.log("just checking commit");
+
+let likeCount = 0;
+
+const button = document.getElementById("likeBtn");
+const countDisplay = document.getElementById("count");
+
+button.addEventListener("click", () => {
+  likeCount++;
+  countDisplay.textContent = "Likes: " + likeCount;
+});
